b6516bed9e038f8fb662fca53bbb2bb4e50eeb20
HSL8696 <73421711+HSL8696@users.noreply.github.com>
2020-10-27 16:21:15 +0800

    Delete example01.java
    第一次用GitHub删除文件

--- example01.java
+++
@@ -1,8 +0,0 @@
-public  class example01
-{
-  public static void main()
-  {
-    Systeam.out.println("hello");
-  Systeam.out.println("world");                                                 ");
-  }
-}
